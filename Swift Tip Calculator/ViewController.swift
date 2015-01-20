83d63c10721a5d64e4dd2c3ff409d2885728d3c5
yozamacs <yozamacs@yahoo.com>
2015-01-19 20:22:16 -0800

    Initial Commit

---
+++ Swift Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Tip Calculator
+//
+//  Created by Maryum Styles on 1/19/15.
+//  Copyright (c) 2015 Maryum Styles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
